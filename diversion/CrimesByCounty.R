a9fd0d8d418754a98809dddbed395ccc55ceccc0
knewbol1 <knewbol1@jhu.edu>
2020-02-04 14:54:12 -0500

    fixed tiling issue

--- diversion/CrimesByCounty.R
+++ diversion/CrimesByCounty.R
@@ -10,23 +10,29 @@ offense2002_2010 <- read_excel("~/Documents/OPI/data/UnlockedDJSData/Offense_200
 offense2011_2019 <- read_excel("~/Documents/OPI/data/UnlockedDJSData/Offense_2011_2019.xlsx")
 
 all_offense <- rbind(offense2002_2010, offense2011_2019)
+all_offense$OFFENSE_DATE <- as.Date(all_offense$OFFENSE_DATE)
+all_offense$year <- year(all_offense$OFFENSE_DATE)
 
-yroffense <- all_offense %>%
-  filter(year(OFFENSE_TYPE) > '2002-01-01') %>%
-  group_by(COUNTY, OFFENSE_DATE, year(OFFENSE_DATE)) %>%
+#Filter by individual (only see their most severe case)
+onecase <- all_offense %>%
+  group_by(REVLEGALINCIDENT_KEY) %>%
+  top_n(-1,FINAL_RANK)
+
+#get number of offenses per year by county
+yroffense <- onecase %>%
+  group_by(COUNTY, year) %>%
   summarize(count = n())
 
-#Error: Invalid input: date_trans works with objects of class Date only
-#I want it to group by year but I can't get that to work...
-countycrimesperyear <- ggplot(yroffense, aes(x=year(OFFENSE_DATE), y=count)) +
+#graph number of offenses by county
+countycrimesperyear <- ggplot(yroffense, aes(x=year(year) , y=count)) +
   geom_tile() + 
   geom_line(color = "#F2CA27", size = 0.1) +
   geom_smooth(color = "#1A1A1A") +
-  scale_x_date(breaks = date_breaks("1 year"), labels = date_format("%y")) +
+  scale_x_continuous(limits = c(2005, 2019)) +
   labs(x = "Date of Arrest", y = "Number of Arrests per Year", 
        title = "Annual Arrests in MD Counties") +
   theme(axis.text.x = element_text(angle=90)) +
-  facet_wrap(~ COUNTY, nrow = 6) +
+  facet_wrap(~ COUNTY, nrow = 6, scales = "free_y") +
   ylim(0,NA)
 
 countycrimesperyear
