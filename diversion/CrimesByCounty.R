c11dc1840331bc732a90dbc6c2410e08d2dff912
knewbol1 <knewbol1@jhu.edu>
2020-02-05 17:04:20 -0500

    caught analysis error, 2019 data only goes until June

--- diversion/CrimesByCounty.R
+++ diversion/CrimesByCounty.R
@@ -19,10 +19,9 @@ onecase <- all_offense %>%
   group_by(REVLEGALINCIDENT_KEY) %>%
   top_n(-1,FINAL_RANK)
 
-#get number of offenses per year by county
-#filtering for marijuana charges (possession < 10 grams decriminalized in 2015)
+#get number of offenses per year by county, only goes until 6/29 for FY19
 yroffense <- onecase %>%
-  filter(year > 2001, year < 2020) %>%
+  filter(year > 2001, year < 2019) %>%
   group_by(COUNTY, year) %>%
   summarize(count = n())
 
@@ -31,7 +30,7 @@ countycrimesperyear <- ggplot(yroffense, aes(x=year, y=count)) +
   geom_tile() + 
   geom_line(color = "#F2CA27", size = 0.1) +
   geom_smooth(color = "#1A1A1A") +
-  scale_x_continuous(limits = c(2002, 2019)) +
+  scale_x_continuous(limits = c(2002, 2018)) +
   labs(x = "Date of Arrest", y = "Number of Arrests per Year", 
        title = "Annual Arrests in MD Counties") +
   theme(axis.text.x = element_text(angle=90)) +
@@ -44,15 +43,11 @@ countycrimesperyear
 countycrimes <- spread(yroffense, year, count)
 
 yearcount <- onecase %>%
-  filter(year > 2001, year < 2020) %>%
+  filter(year > 2001, year < 2019) %>%
   group_by(year) %>%
   summarize(count = n())
 
 yearcount <- spread(yearcount, year, count)
-
-percent_change <- function(year1, year2) {
-  return((year2 - year1) / year1)
-}
 
 #Percent Change Table
 change <- countycrimes %>% select(COUNTY)
@@ -63,11 +58,11 @@ change$'2007-2012 (% Change)' <- round((((countycrimes$'2012' - countycrimes$'20
                                                  countycrimes$'2007') *100), digits=2)
 change$'2012-2017 (% Change)' <- round((((countycrimes$'2017' - countycrimes$'2012') / 
                                                  countycrimes$'2012') *100), digits=2)
-change$'2015-2016 (% Change)' <- round((((countycrimes$'2016' - countycrimes$'2015') / 
-                                           countycrimes$'2015') *100), digits=2)
-change$'2017-2019 (% Change)' <- round((((countycrimes$'2019' - countycrimes$'2017') / 
+#change$'2015-2016 (% Change)' <- round((((countycrimes$'2016' - countycrimes$'2015') / 
+                                          # countycrimes$'2015') *100), digits=2)
+change$'2017-2018 (% Change)' <- round((((countycrimes$'2018' - countycrimes$'2017') / 
                                                  countycrimes$'2017') *100), digits=2)
-change$'Total % Change' <- round((((countycrimes$'2019' - countycrimes$'2002') / 
+change$'Total % Change' <- round((((countycrimes$'2018' - countycrimes$'2002') / 
                                            countycrimes$'2002') *100), digits=2)
 
 formattable(change, align = c("l", rep("c", NCOL(change) - 1)))
