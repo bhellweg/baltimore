401d2fd1597c2cf78c213fd718457e1b9b9932a3
knewbol1 <knewbol1@jhu.edu>
2020-02-04 19:49:22 -0500

    fixed minor error in plot

--- diversion/CrimesByCounty.R
+++ diversion/CrimesByCounty.R
@@ -10,7 +10,7 @@ offense2002_2010 <- read_excel("~/Documents/OPI/data/UnlockedDJSData/Offense_200
 offense2011_2019 <- read_excel("~/Documents/OPI/data/UnlockedDJSData/Offense_2011_2019.xlsx")
 
 all_offense <- rbind(offense2002_2010, offense2011_2019)
-all_offense$OFFENSE_DATE <- as.Date(all_offense$OFFENSE_DATE)
+#all_offense$OFFENSE_DATE <- as.Date(all_offense$OFFENSE_DATE)
 all_offense$year <- year(all_offense$OFFENSE_DATE)
 
 #Filter by individual (only see their most severe case)
@@ -24,7 +24,7 @@ yroffense <- onecase %>%
   summarize(count = n())
 
 #graph number of offenses by county
-countycrimesperyear <- ggplot(yroffense, aes(x=year(year) , y=count)) +
+countycrimesperyear <- ggplot(yroffense, aes(x=year, y=count)) +
   geom_tile() + 
   geom_line(color = "#F2CA27", size = 0.1) +
   geom_smooth(color = "#1A1A1A") +
