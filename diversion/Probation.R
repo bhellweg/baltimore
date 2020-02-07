41752440f5f4e8abcde6661285f8cd14189408eb
knewbol1 <knewbol1@jhu.edu>
2020-02-07 11:57:12 -0500

    trends in probation frequency across all MD counties

---
+++ diversion/Probation.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+library(readxl)
+library(formattable)
+library(lubridate)
+library(leaflet)
+library(dplyr)
+library(scales)
+library(formattable)
+library(delt)
+
+offense1 <- read_excel("~/Documents/OPI/data/UnlockedDJSData/Offense_2002_2010.xlsx")
+offense2 <- read_excel("~/Documents/OPI/data/UnlockedDJSData/Offense_2011_2019.xlsx")
+all_offense <- rbind(offense1, offense2)
+
+disposition1 <- read_excel("~/Documents/OPI/data/UnlockedDJSData/Disposition_2002_2010.xlsx")
+disposition2 <- read_excel("~/Documents/OPI/data/UnlockedDJSData/Disposition_2011_2019.xlsx")
+all_disposition <- rbind(disposition1, disposition2)
+
+demographics <- read_excel("~/Documents/OPI/data/UnlockedDJSData/Demographics_2002_2019.xlsx")
+
+all_offense$REVLEGALINCIDENT_KEY <- as.numeric(all_offense$REVLEGALINCIDENT_KEY)
+all_disposition$REVLEGALINCIDENT_KEY <- as.numeric(all_disposition$REVLEGALINCIDENT_KEY)
+demographics$REVLEGALINCIDENT_KEY <- as.numeric(demographics$REVLEGALINCIDENT_KEY)
+
+#merge all tables together
+offdisp <- left_join(all_offense, all_disposition, by = c("REVACTOR_ID","REVLEGALINCIDENT_KEY"))
+all_djs <- left_join(offdisp,demographics,by = c("REVACTOR_ID","REVLEGALINCIDENT_KEY"))
+
+#Get data between 2002-01-01 and 2018-12-31
+all_djs <- filter(all_djs, year(all_djs$COMPLAINT_DATE.x) > 2001)
+all_djs <- filter(all_djs, year(all_djs$COMPLAINT_DATE.x) < 2019)
+
+#all offenses ending in probation
+probation <- filter(all_djs, grepl("Probation", all_djs$DISPOSITION_TEXT))
+probation$year <- year(probation$COMPLAINT_DATE.x)
+
+yrprobations <- probation %>%
+  filter(year > 2001, year < 2019) %>%
+  group_by(COUNTY, year) %>%
+  summarize(count = n())
+
+#tile by county, number of probations per year
+probationperyear <- ggplot(yrprobations, aes(x=year, y=count)) +
+  geom_tile() +
+  geom_line(color = "#F2CA27", size = 0.1) +
+  geom_smooth(color = "#1A1A1A") +
+  scale_x_continuous(limits = c(2002, 2018)) +
+  labs(x = "Year", y = "Number of Probations", 
+       title = "Annual Probations in MD Counties") +
+  facet_wrap(~ COUNTY, nrow = 6, scales = "free_y") +
+  ylim(0,NA)
+probationperyear
+
