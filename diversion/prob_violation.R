2560425b06130e3006bf5d5dd8c747124fc0acf2
knewbol1 <knewbol1@jhu.edu>
2020-03-04 16:53:47 -0500

    created recidivism function

--- diversion/prob_violation.R
+++ diversion/prob_violation.R
@@ -64,7 +64,7 @@ top_djs <- top_disp %>%
 top_djs <- left_join(top_djs,demographics, by=c("REVACTOR_ID","REVLEGALINCIDENT_KEY")) 
 
 #Filter for DJS data we want
-top_djs <- filter(top_djs, year(top_djs$COMPLAINT_DATE.x) > 2009)
+top_djs <- filter(top_djs, year(top_djs$COMPLAINT_DATE.x) > 2005)
 top_djs <- filter(top_djs, year(top_djs$COMPLAINT_DATE.x) < 2020)
 #---------------------------Completed DJS table------------------------------
 #Filtered for just Baltimore because the entire dataset took too long
@@ -95,39 +95,194 @@ order <- function(arg1, arg2) {
 
 djs$arrestnum <- mapply(FUN = order, djs$REVACTOR_ID, djs$COMPLAINT_DATE.x)
 
-#Based on the Data Resource Guide from DJS, the average length of stay on probation is 450 days. 
-#This will examine probation violations that occur within one year of the initial adjudication.
-
-#Find all sustained offenses that got probation
-probations <- djs %>% 
-  filter(ADJ_DECISION_CODE == "S") %>%
-  filter(grepl("Probation|Supervision", DISPOSITION_TEXT))
-
-#Find an individuals first arrest that resulted in sustained probation
-indiv_prob <- probations %>%
-  select(REVACTOR_ID, COMPLAINT_DATE.x) %>%
-  group_by(REVACTOR_ID) %>%
-  arrange(COMPLAINT_DATE.x) %>%
-  filter(row_number() == 1) %>%
-  ungroup()
-indiv_prob <- rename(indiv_prob, probationstart=COMPLAINT_DATE.x)
-
-updated_djs <- left_join(djs, indiv_prob)
-updated_djs <- updated_djs %>%
-  mutate(dayssinceprobation = difftime(COMPLAINT_DATE.x, probationstart, units = "days"))
-
-post_probation <- updated_djs %>% filter(dayssinceprobation > 0) %>% filter(grepl("VIOP", OFFENSE_CODE))
-
-viop <- post_probation %>% group_by(dayssinceprobation) %>% summarize(count = n()) %>% rename(total=count)
+#We don't want to use data where someone committed a crime resulting in probation before 2005
+#and they violated it after 2005 because we can't track days since offense
+clean_djs <- djs %>% filter(!(OFFENSE_CODE == "VIOP" && arrestnum == 1))
 
 
-#Graph results
-probation_violations <- ggplot(viop, aes(x=as.numeric(dayssinceprobation))) +
-  geom_histogram(color="black", fill="lightblue", binwidth = 50) +
-  scale_x_continuous(limits=c(0, 700), breaks=c(0, 100, 200, 300, 400, 500, 600, 700)) +
-  labs(x="Days since initial probation decision", y="Count", title="Violation of Probation Frequency, Baltimore City 2010-2019")
+recidivism <- function(filter, criteria) {
+  if (filter == "violation") {
+    filtered_table <- clean_djs %>%
+      filter(grepl(criteria, OFFENSE_TEXT))
+  } else if (filter == "age") {
+    filtered_table <- clean_djs %>%
+      filter(AGE_COMPLAINT == criteria)
+  } else if (filter == "county") {
+    filtered_table <- clean_djs %>%
+      filter(COUNTY == criteria)
+  } else if (filter == "charge") {
+    filtered_table <- clean_djs %>%
+      filter(OFFENSE_TYPE == criteria)
+  } else {
+    filtered_table <- clean_djs
+  }
   
-probation_violations
+  #Resolved at Intake
+  crai <- filtered_table %>%
+    filter(DETNDECIDE_DEC == "Resolved/No Jurisdiction") %>%
+    group_by(REVACTOR_ID) %>%
+    mutate(firstcrime = ifelse(row_number() == 1, 1, 0))
   
+  first_crai <- crai %>%
+    filter(firstcrime == 1) %>%
+    select(REVACTOR_ID, COMPLAINT_DATE.x) %>%
+    rename(firstdate = COMPLAINT_DATE.x)
+  
+  crai_djs <- left_join(clean_djs, first_crai)
+  
+  crai_reoffend <- crai_djs %>% mutate(reoffense = ifelse(COMPLAINT_DATE.x > firstdate, 1, 0))
+  crai_reoffenders <- crai_reoffend %>%
+    filter(reoffense == 1) %>%
+    group_by(REVACTOR_ID) %>%
+    summarise(count = n())
+  
+  percent_crai_reoffense <- nrow(crai_reoffenders)/nrow(first_crai)
+  #------------------------end resolved at intake-----------------
+  
+  #Informaled
+  informaled <- filtered_table %>%
+    filter(DETNDECIDE_DEC == "Informaled") %>%
+    group_by(REVACTOR_ID) %>%
+    mutate(firstcrime = ifelse(row_number() == 1, 1, 0))
+  
+  first_informaled <- informaled %>%
+    filter(firstcrime == 1) %>%
+    select(REVACTOR_ID, COMPLAINT_DATE.x) %>%
+    rename(firstdate = COMPLAINT_DATE.x)
+  
+  informaled_djs <- left_join(clean_djs, first_informaled)
+  
+  informaled_reoffend <- informaled_djs %>% mutate(reoffense = ifelse(COMPLAINT_DATE.x > firstdate, 1, 0))
+  informaled_reoffenders <- informaled_reoffend %>%
+    filter(reoffense == 1) %>%
+    group_by(REVACTOR_ID) %>%
+    summarise(count = n())
+  
+  percent_informaled_reoffense <- nrow(informaled_reoffenders)/nrow(first_informaled)
+  #------------------end informaled-------------------------------
+  
+  #Sustained
+  sus <- filtered_table %>%
+    filter(ADJ_DECISION_CODE == "S") %>%
+    group_by(REVACTOR_ID) %>%
+    arrange(COMPLAINT_DATE.x) %>%
+    mutate(firstcrime = ifelse(row_number() == 1, 1, 0))
+  
+  first_sus <- sus %>%
+    filter(firstcrime == 1) %>%
+    select(REVACTOR_ID, COMPLAINT_DATE.x) %>%
+    rename(firstdate = COMPLAINT_DATE.x)
+  
+  sus_djs <- left_join(clean_djs, first_sus)
+  
+  sus_reoffend <- sus_djs %>% mutate(reoffense = ifelse(COMPLAINT_DATE.x > firstdate, 1, 0))
+  sus_reoffenders <- sus_reoffend %>%
+    filter(reoffense == 1) %>%
+    group_by(REVACTOR_ID) %>%
+    summarise(count = n())
+  
+  percent_sustained_reoffense <- nrow(sus_reoffenders)/nrow(first_sus)
+  #---------------------end sustained------------------------
+  
+  #Unsustained
+  unsus <- filtered_table %>%
+    filter(ADJ_DECISION_CODE == "U") %>%
+    group_by(REVACTOR_ID) %>%
+    arrange(COMPLAINT_DATE.x) %>%
+    mutate(firstcrime = ifelse(row_number() == 1, 1, 0))
+  
+  first_unsus <- unsus %>%
+    filter(firstcrime == 1) %>%
+    select(REVACTOR_ID, COMPLAINT_DATE.x) %>%
+    rename(firstdate = COMPLAINT_DATE.x)
+  
+  unsus_djs <- left_join(clean_djs, first_unsus)
+  
+  unsus_reoffend <- unsus_djs %>% mutate(reoffense = ifelse(COMPLAINT_DATE.x > firstdate, 1, 0))
+  unsus_reoffenders <- unsus_reoffend %>%
+    filter(reoffense == 1) %>%
+    group_by(REVACTOR_ID) %>%
+    summarise(count = n())
+  
+  percent_unsustained_reoffense <- nrow(unsus_reoffenders)/nrow(first_unsus)
+  #--------------end unsustained----------------------
+  
+  #Probation
+  prob <- filtered_table %>%
+    filter(ADJ_DECISION_CODE == "S") %>%
+    filter(grepl("Probation|Supervision", DISPOSITION_TEXT)) %>%
+    group_by(REVACTOR_ID) %>%
+    mutate(firstcrime = ifelse(row_number() == 1, 1, 0))
+  
+  first_prob <- prob %>%
+    filter(firstcrime == 1) %>%
+    select(REVACTOR_ID, COMPLAINT_DATE.x) %>%
+    rename(firstdate = COMPLAINT_DATE.x)
+  
+  prob_djs <- left_join(clean_djs, first_prob)
+  
+  prob_reoffend <- prob_djs %>% mutate(reoffense = ifelse(COMPLAINT_DATE.x > firstdate, 1, 0))
+  prob_reoffenders <- prob_reoffend %>%
+    filter(reoffense == 1) %>%
+    group_by(REVACTOR_ID) %>%
+    summarise(count = n())
+  
+  percent_probation_reoffense <- nrow(prob_reoffenders)/nrow(first_prob)
+  #-------------------end probation---------------------------
+  
+  #Committed
+  commit <- filtered_table %>%
+    filter(ADJ_DECISION_CODE == "S") %>%
+    filter(grepl("Commit", DISPOSITION_TEXT)) %>%
+    group_by(REVACTOR_ID) %>%
+    mutate(firstcrime = ifelse(row_number() == 1, 1, 0))
+  
+  first_commit <- commit %>%
+    filter(firstcrime == 1) %>%
+    select(REVACTOR_ID, COMPLAINT_DATE.x) %>%
+    rename(firstdate = COMPLAINT_DATE.x)
+  
+  commit_djs <- left_join(clean_djs, first_commit)
+  
+  commit_reoffend <- commit_djs %>% mutate(reoffense = ifelse(COMPLAINT_DATE.x > firstdate, 1, 0))
+  commit_reoffenders <- commit_reoffend %>%
+    filter(reoffense == 1) %>%
+    group_by(REVACTOR_ID) %>%
+    summarise(count = n())
+  
+  percent_commit_reoffense <- nrow(commit_reoffenders)/nrow(first_commit)
+  #----------end commit--------------------
+  
+  reoffense <- data.frame("ResolvedAtIntake"=percent_crai_reoffense, 
+                          "Informaled" = percent_informaled_reoffense, 
+                          "Sustained" = percent_sustained_reoffense,
+                          "Unsustained" = percent_unsustained_reoffense,
+                          "Probation" = percent_probation_reoffense,
+                          "Commitment" = percent_commit_reoffense)
+  reoffense_table <- gather(reoffense, "Outcome", "Probability")
+  
+  reoffense_graph <- ggplot(reoffense_table, aes(x=reorder(Outcome,Probability))) +
+    geom_line(aes(y=as.numeric(reoffense_table$Probability)), group = 1) +
+    geom_point(aes(y=as.numeric(reoffense_table$Probability))) +
+    scale_x_discrete() +
+    labs(x="Outcome", y="Probability of reoffense", title="Probability of reoffense based on interaction outcome")
+  reoffense_graph
+}
+
+#Possible options for recidivism function
+#filters: violation, age, charge, county or all
+#violation: select the offense_text that you want to see (must match spelling/capitalization)
+#age: 10-17 
+#charge: Felony or Misdemeanor (must be capitalized)
+#county: currently only deals with Baltimore City but could be expanded to other counties
+
+recidivism("all", "all")
+
+
+
+
+
+
+
 
 
